12720e5d0c4d5ebde4ff162dbc711659dbcfaced
Dardan <dardan@uvd.co.uk>
2013-10-18 11:23:49 +0000

    added a couple comments in index.php

--- src/index.php
+++ src/index.php
@@ -10,8 +10,10 @@ $grid = new Grid(5, 10);
 $controller = new Controller("ffrff", new MarsRover(0, 0), $grid);
 $controller->controlRover();
 
+//change the cell from '0' to 'X' wherever the rover has landed on after following commands
 $grid->grid[$controller->rover->getX()][$controller->rover->getY()]->setState('X');
 
+//print the grid of cells
 for ($x=$grid->width; $x > 0; $x--) { 
 	for ($y=0; $y < $grid->height; $y++) { 
 		print $grid->grid[$x -1][$y]->getState();
